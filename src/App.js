4dfc61864d9ec7b08c9a76fa69d87f38deb7c2d7
Rahmatxonov <rahmatxonov1001@gmail.com>
2023-11-22 18:13:41 +0500

    modalka

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import "./App.css";
 import Navbar from "./components/navbar/navbar";
+import Modal from "./components/modalka/modal";
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <div className="App">
       <Navbar />
+      <div className="openModal">
+        <h1 className="openModalTitle">Hello world</h1>
+        <button
+          className="openModalBtn"
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        >
+          Open
+        </button>
+        {openModal && <Modal closeModal={setOpenModal} />}
+      </div>
     </div>
   );
 }
