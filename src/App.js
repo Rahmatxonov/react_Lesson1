6d622d82e2cc1a7662dbcd17b1cd4b37cfe98ee5
Rahmatxonov <rahmatxonov1001@gmail.com>
2023-11-23 18:37:46 +0500

    news

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import React from "react";
-import { useState } from "react";
-import "./App.css";
-import Navbar from "./components/navbar/navbar";
-import Modal from "./components/modalka/modal";
-import Random from "./components/random/random";
-
 function App() {
-  const [openModal, setOpenModal] = useState(false);
-
   return (
     <div className="App">
-      <Navbar />
-      <div className="openModal">
-        <h1 className="openModalTitle">Hello world</h1>
-        <button
-          className="openModalBtn"
-          onClick={() => {
-            setOpenModal(true);
-          }}
-        >
-          Open
-        </button>
-        {openModal && <Modal closeModal={setOpenModal} />}
-      </div>
-      <h2 className="randomTitle">Random Images</h2>
-      <div className="image-container">
-        <Random />
-      </div>
+      <h1>Hello World</h1>
     </div>
   );
 }
