fc6f6165ee700e4bc5009cd4ddf3c30540303100
Rahmatxonov <rahmatxonov1001@gmail.com>
2023-11-30 00:25:59 +0500

    news

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import FetchCountries from "./components/FetchCountries";
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import ArticleList from "./components/ArticlesList";
+import ArticleDetails from "./components/ArticleDetails";
 
 function App() {
   return (
-    <div className="container">
-      <FetchCountries />;
-    </div>
+    <Router>
+      <Route exact path="/" component={ArticleList} />
+      <Route path="/article/:id" component={ArticleDetails} />
+    </Router>
   );
 }
 
---
+++ src/components/ArticlesList.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+// import "./ArticleList.css";
+
+const ArticleList = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.spaceflightnewsapi.net/v3/articles"
+        );
+        setArticles(response.data);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  return (
+    <div>
+      <h1>Spaceflight News</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <Link to={`/article/${article.id}`}>{article.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ArticleList;
