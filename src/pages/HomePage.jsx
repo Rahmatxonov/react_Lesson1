24abc84a1b8c482b53f176209176ddad3fd792b6
Rahmatxonov <rahmatxonov1001@gmail.com>
2023-12-08 11:19:39 +0500

    todo List useMemo, useCallback, useReducer

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import { useRef, useState } from "react";
+
+// import Tab from "react-bootstrap/Tab";
+// import Tabs from "react-bootstrap/Tabs";
+
+// import TodoItem from "../components/TodoItem";
+
+const HomePage = () => {
+  return <></>;
+};
+
+export default HomePage;
